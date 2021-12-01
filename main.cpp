7d95289dd445174ecb84f1d2330431996cfe5587
James Horne <james.m.horne.mil@army.mil>
2021-12-01 11:12:16 -0500

    Tutorial video 2 -- Create menu system

--- main.cpp
+++ main.cpp
@@ -1,8 +1,16 @@
 #include <Windows.h>
 
+#define FILE_MENU_NEW  1
+#define FILE_MENU_OPEN 2
+#define FILE_MENU_EXIT 3
+
 #pragma comment(lib, "user32.lib")
 
 LRESULT CALLBACK WindowProcedure(HWND, UINT, WPARAM, LPARAM);
+
+void AddMenus(HWND);
+
+HMENU hMenu;
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hprevInstance, LPSTR args, int ncmdshow)
 {
@@ -40,10 +48,52 @@ LRESULT CALLBACK WindowProcedure(HWND hWnd, UINT msg, WPARAM wp, LPARAM lp)
             PostQuitMessage(0);
         break;
 
+        case WM_CREATE:
+            AddMenus(hWnd);
+        break;
+
+        case WM_COMMAND:
+            switch (wp)
+            {
+                case FILE_MENU_EXIT:
+                    DestroyWindow(hWnd);
+                break;
+
+                case FILE_MENU_NEW:
+                    MessageBeep(MB_NOFOCUS);
+                break;
+
+                case FILE_MENU_OPEN:
+                    MessageBeep(MB_OK);
+                break;
+            }
+        break;
+
         default:
             return DefWindowProcW(hWnd, msg, wp, lp);
     }
 
+}
+
+void AddMenus(HWND hWnd)
+{
+    hMenu = CreateMenu();
+    HMENU hFileMenu = CreateMenu(); // file menu popup
+    HMENU hSubMenu  = CreateMenu();
+
+    AppendMenu(hSubMenu, MF_STRING, NULL, "SubMenu Item");
+
+    AppendMenu(hFileMenu, MF_STRING, FILE_MENU_NEW, "New");
+    AppendMenu(hFileMenu, MF_STRING, FILE_MENU_OPEN, "Open");
+    AppendMenu(hFileMenu, MF_POPUP, (UINT_PTR) hSubMenu, "Options");
+    AppendMenu(hFileMenu, MF_SEPARATOR, NULL, NULL);
+    AppendMenu(hFileMenu, MF_STRING, FILE_MENU_EXIT, "Exit");
+
+    // AppendMenu(hMenu, MF_STRING, 1, "File");  // used when just having a string menu button. WM_COMMAND message generated
+    AppendMenu(hMenu, MF_POPUP, (UINT_PTR) hFileMenu, "File");
+    AppendMenu(hMenu, MF_STRING, NULL, "Help");
+
+    SetMenu(hWnd, hMenu);
 }
 
 /*** end of file ***/
@@ -52,4 +102,5 @@ LRESULT CALLBACK WindowProcedure(HWND hWnd, UINT msg, WPARAM wp, LPARAM lp)
     References
 
     https://www.youtube.com/watch?v=8GCvZs55mEM&t=68s
+    https://www.youtube.com/watch?v=7K6HCeog09c
 */

147ddd252dcc5e6eec02a10ffc7d73bad36419bc
James Horne <james.m.horne.mil@army.mil>
2021-12-01 10:06:17 -0500

    Added code for a basic window

--- main.cpp
+++ main.cpp
@@ -2,9 +2,54 @@
 
 #pragma comment(lib, "user32.lib")
 
+LRESULT CALLBACK WindowProcedure(HWND, UINT, WPARAM, LPARAM);
+
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hprevInstance, LPSTR args, int ncmdshow)
 {
-    MessageBox(NULL, "Hello, World", "Hello, World", MB_OK);
+    WNDCLASSW wc = {0};
+
+    wc.hbrBackground = (HBRUSH) COLOR_WINDOW;
+    wc.hCursor       = LoadCursor(NULL, IDC_ARROW);
+    wc.hInstance     = hInstance;
+    wc.lpszClassName = L"myWindowClass";
+    wc.lpfnWndProc   = WindowProcedure;
+
+    if(!RegisterClassW(&wc))
+    {
+        return -1;
+    }
+
+    CreateWindowW(L"myWindowClass", L"My Window", WS_OVERLAPPEDWINDOW | WS_VISIBLE, 100, 100, 500, 500, NULL, NULL, NULL, NULL);
+
+    MSG msg = {0};
+
+    while (GetMessage(&msg, NULL, NULL, NULL))
+    {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
 
     return 0;
 }
+
+LRESULT CALLBACK WindowProcedure(HWND hWnd, UINT msg, WPARAM wp, LPARAM lp)
+{
+    switch (msg)
+    {
+        case WM_DESTROY:
+            PostQuitMessage(0);
+        break;
+
+        default:
+            return DefWindowProcW(hWnd, msg, wp, lp);
+    }
+
+}
+
+/*** end of file ***/
+
+/*
+    References
+
+    https://www.youtube.com/watch?v=8GCvZs55mEM&t=68s
+*/
